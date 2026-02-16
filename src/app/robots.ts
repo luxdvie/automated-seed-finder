e549b9de095f24c7e61f2b9329d570bd20359244
Austin Brown <austinbrown2500@gmail.com>
2026-02-15 19:32:38 -0500

    skill sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,14 +1,10 @@
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL
-  const sitemap = siteUrl ? `${siteUrl.replace(/\/$/, '')}/sitemap.xml` : undefined
-
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap,
   }
 }
